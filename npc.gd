77b67c765d71bb9251c3a5f68f45bacf5e749f26
Matej <matej.hl@gmail.com>
2023-04-29 19:34:22 +0200

    NPC move

--- npc.gd
+++ npc.gd
@@ -1,13 +1,14 @@
-extends RigidBody2D
+extends RigidBody3D
 
 func _ready():
-	%Agent.connect("velocity_computed", velocity_computed)
+	%Agent.target_position = get_tree().get_nodes_in_group("starbucks")[0].global_position
 
 func _process(delta):
 	var speed = 300
-	%Agent.target_location = get_tree().get_nodes_in_group("starbucks")[0].global_position
-	%Agent.set_velocity(global_position.direction_to(%Agent.get_next_location())*delta*speed)
+	%Agent.set_velocity(global_position.direction_to(%Agent.get_next_path_position())*delta*speed)
 #	position += global_position.direction_to(%Agent.get_next_location())
 
 func velocity_computed(vel):
+	vel.y = 0
 	apply_central_impulse(vel)
+	$Pivot.look_at(global_position + vel)
--- player.gd
+++ player.gd
@@ -1,32 +1,33 @@
 extends CharacterBody3D
 
-@export var speed = 14.0
-@export var fall_acceleration = 75
+@export var speed = 2.0
+@export var fall_acceleration = 10
 var target_velocity = Vector3.ZERO
 
 func _physics_process(delta):
 	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down").normalized()
 	direction = Vector3(direction.x, 0, direction.y)
 	if direction != Vector3.ZERO:
+		# TODO make smooth
 		$Pivot.look_at(position + direction, Vector3.UP)
+		#footsteps sound
+		if $Timer.time_left <=0:
+			$SFXFootsteps.pitch_scale = randf_range(0.8,1.2)
+			$SFXFootsteps.play()
+			$Timer.start(0.35)
 		
 	# Ground Velocity
 	target_velocity.x = direction.x * speed
 	target_velocity.z = direction.z * speed
 
 	# Vertical Velocity
-	if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
-		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
+#	if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
+#		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
 
 	# Moving the Character
-	velocity = target_velocity
+	velocity /= 70 * delta
+	velocity += target_velocity
 	move_and_slide()
-
-	#footsteps sound
-	if $Timer.time_left <=0:
-		$SFXFootsteps.pitch_scale = randf_range(0.8,1.2)
-		$SFXFootsteps.play()
-		$Timer.start(0.8)
 
 
 #	$Node2D.look_at(get_global_mouse_position())
