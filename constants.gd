790f8b87df1ddee06397dc23f88fd5828e5f092d
Matej <matej.hl@gmail.com>
2023-04-30 14:05:38 +0200

    asd

---
+++ constants.gd
@@ -0,0 +1,5 @@
+extends Node
+
+const FOOD_SPOTS = ["pizza", "texmex", "pho"]
+const WORK_SPOTS = ["green office", "red office", "blue office"]
+const HOME_SPOTS = ["green apartment", "red apartment", "blue apartment"]
