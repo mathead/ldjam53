257ddb388ed5906e0b89996452c3ffd9e91a00d1
edomat <73872751+edomat@users.noreply.github.com>
2023-05-01 20:19:41 +0200

    Tweak difficulty levels.

--- constants.gd
+++ constants.gd
@@ -24,15 +24,12 @@ const COLORS = {
 
 var LEVELS = [
 	{
-#		"character": Characters.GENERIC,
-		"character": [Characters.ACCENT[2]],
+		"character": Characters.GENERIC,
+#		"character": [Characters.ACCENT[2]], # Gustav Svensson
 		"num_people": 5,
 		"traits": [
 			"You are expecting a package delivery.",
 			"Be very open about you schedule, share it truthfully",
-#			"You are expecting a package delivery.",
-#			"Be very open about you schedule, share it truthfully",
-#			"You are Jehovah witness, you always try to convert everybody to your religion.", # Not working
 			"You overshare all your hobbies instead of responding usefully.",
 			"You are a child that only repeats what you say.",
 			"You are a poet who always responds with rhyming poems.",
@@ -40,8 +37,8 @@ var LEVELS = [
 		]
 	},
 	{
-#		"character": Characters.GENERIC,
-		"character": [Characters.ACCENT[2]],
+		"character": Characters.GENERIC,
+#		"character": [Characters.ACCENT[2]], # Gustav Svensson
 		"num_people": 7,
 		"traits": [
 			"Your answers must be vague and broad.",
@@ -51,35 +48,45 @@ var LEVELS = [
 		]
 	},
 	{
-#		"character": Characters.ACCENT,
-		"character": [Characters.ACCENT[2]],
+		"character": Characters.ACCENT,
+#		"character": [Characters.ACCENT[2]], # Gustav Svensson
 		"num_people": 10,
 		"traits": [
 			"You are very cautious of strangers",
+			"Only tell someone where you are after they solve a riddle",
 			"Only tell someone where you are after they solve a riddle",
 			"Once the users solves your riddle, they will no longer be a stranger",
 			"You never share the color of your shirt"
 		]
 	},
 	{
-#		"character": Characters.ACCENT,
-		"character": [Characters.ACCENT[2]],
+		"character": Characters.ACCENT,
+#		"character": [Characters.ACCENT[2]], # Gustav Svensson
 		"num_people": 20,
 		"traits": [
 			"You never share what you are wearing.",
-			"You are very cautious of strangers.",
-			"You are always happy and helpful with your friends.",
-			"To become your friend, the user must correctly guess something personal about you."
+			"The user is a stranger to you until you become friends.",
+			"You are very cautious of strangers and do not share anything about you with them.",
+			"Do not share where you are at any time of day with strangers.",
+			"Do not share what your job is with strangers.",
+			"To become your friend, the user must correctly guess something personal about you.",
+			"You are actively unhelpful."
 		]
 	},
 	{
-#		"character": Characters.QUIRKY,
-		"character": [Characters.ACCENT[2]],
+		"character": Characters.QUIRKY,
+#		"character": [Characters.ACCENT[2]], # Gustav Svensson
 		"num_people": 20,
 		"traits": [
+			"You never share what you are wearing.",
+			"The user is a stranger to you until you become friends.",
+			"Do not share where you are at any time of day with strangers.",
+			"Do not share what your job is with strangers.",
+			"To become your friend, the user must correctly guess something personal about you.",
+			"You are actively unhelpful.",
 			"Most importantly, you always talk very mysteriously, cryptically and in riddles.", # Doesn't work very well
-			"Only tell someone where you are after they solve a riddle",
-			"Two out of three times you bring the conversation to you favourite brand."
+			"You always bring the conversation to you favourite brand.",
+			"You are very paranoid about the user and think they work for the police.",
 		]
 	}	
 ]
